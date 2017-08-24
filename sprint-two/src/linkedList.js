aa0030d9945c7eb20182b84e60457af65353dde7
tiffaphm <tiffanynpham@gmail.com>
2017-08-23 20:53:34 -0700

    Passed all tests for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.tail) {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    } else {
+      list.tail = Node(value);
+      list.head = list.tail;
+    }
   };
 
   list.removeHead = function() {
+    var currentHead = list.head.value;
+    list.head = list.head.next;
+    return currentHead;
   };
 
   list.contains = function(target) {
+    var currPos = list.head;
+    while (currPos !== null) {
+      if (currPos.value === target) {
+        return true;
+      } else {
+        currPos = currPos.next;
+      }
+    }
+    return false;
   };
 
   return list;
