d2e5d65bba361e4e289f80e86b30c40f81b0003d
tiffaphm <tiffanynpham@gmail.com>
2017-08-24 18:21:11 -0700

    Passing all tests for 'tree'

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,11 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  newTree.children = [];
+  
   _.extend(newTree, treeMethods);
-
-  // your code here
-  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -13,13 +11,45 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  this.children.push({value});
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  // input: target, value
+  // output: true/false
+  // constraints: childrenArray.length > 0
+  
+  // if (children.length > 0)
+  // iterate through the children array
+    // check for value === target
+      // return true
+    // else
+      // redo the function(children)
+  var result = false;
+
+  var testChildren = function(child) {
+    if (child.length > 0) {
+      for (var i = 0; i < child.length; i++) {
+        if (child[i].value === target) {
+          result = true;
+        } else {
+          testChildren(child[i].children);
+        }
+      }
+    }
+  };
+  
+  testChildren(this.children);
+  
+  return result;
 };
 
-
+      // if (child[0].value === target) {
+      //   return true;
+      // } else {
+      //   console.log(child[0].children);
+      //   return testChildren(child[0].children);
+      // }
 
 /*
  * Complexity: What is the time complexity of the above functions?
