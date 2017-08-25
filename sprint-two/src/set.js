20a6bdf5116d28dd6dab3d870ec4a85adcb191f1
tiffaphm <tiffanynpham@gmail.com>
2017-08-25 10:26:12 -0700

    pass all tests for Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,21 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  return this.storage.hasOwnProperty(item);
 };
 
 setPrototype.remove = function(item) {
+  delete this.storage[item];
 };
 
 /*
